2958b2641975530485b8b48b4f55d7b89f1959c6
Tony Ramírez <tonyram9906@outlook.com>
2022-07-26 19:05:28 -0500

    Tasks component

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,12 @@
+const Tasks = ({ tasks }) => {
+  return (
+      // setTasks([...tasks, {}])
+      <>
+        {tasks.map((task) => (
+          <h3 key={task.id}>{task.text}</h3>
+        ))}
+      </>
+    )
+}
+
+export default Tasks
